2086d621b1bdfcf68e599c3e28561dd9c842e484
Alexandr-sys2024 <time2016slot@gmail.com>
2024-09-18 23:10:05 +0300

    addclass Monster

--- main.py
+++ main.py
@@ -22,3 +22,33 @@ class Axe(Weapon):
     def attack(self):
         return "наносит удар топором"
 
+
+# Класс Monster
+class Monster:
+    def __init__(self, health):
+        self.health = health
+        self.health
+        self.h
+
+    def take_damage(self, damage):
+        self.health -= damage
+        self.he
+
+        if self.health <= 0:
+            return "Монстр побежден!"
+            return f"У монстра осталось {self.health} здоровья."
+
+
+# Шаг 3: Класс Fighter
+class Fighter:
+    def __init__(self, name, weapon: Weapon):
+        self.name = name
+        self.weapon = weapon
+
+    def change_weapon(self, new_weapon: Weapon):
+        self.weapon = new_weapon
+
+    def attack(self, monster: Monster):
+        print(f"{self.name} {self.weapon.attack()}")
+        print(monster.take_damage(10))
+
